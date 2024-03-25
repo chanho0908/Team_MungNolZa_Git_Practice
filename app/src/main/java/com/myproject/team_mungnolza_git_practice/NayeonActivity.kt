a16ad4e2f68e36aaffe3fa05eb40b0441b2ccd44
cny1213 <cny1213@gmail.com>
2024-03-25 16:40:49 +0900

    [#12] 나연 연습

---
+++ app/src/main/java/com/myproject/team_mungnolza_git_practice/NayeonActivity.kt
@@ -0,0 +1,11 @@
+package com.myproject.team_mungnolza_git_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NayeonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nayeon)
+    }
+}
