fddb2ad8d7dc1136602d0de01f934e81a327ebde
HeartHyun <dalek1568@gmail.com>
2024-03-25 16:40:54 +0900

    [#13] 정현 Activity 커밋

---
+++ app/src/main/java/com/myproject/team_mungnolza_git_practice/JeoungHyunActivity.kt
@@ -0,0 +1,11 @@
+package com.myproject.team_mungnolza_git_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JeoungHyunActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jeoung_hyun)
+    }
+}
