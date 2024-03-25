b24fd87a3cd8715dac7b4c83fe911c6fac510d74
cny1213 <cny1213@gmail.com>
2024-03-25 17:43:38 +0900

    [#27] 은우의 연습

---
+++ app/src/main/java/com/myproject/team_mungnolza_git_practice/Eunwoo2Activity.kt
@@ -0,0 +1,11 @@
+package com.myproject.team_mungnolza_git_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Eunwoo2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eunwoo2)
+    }
+}
